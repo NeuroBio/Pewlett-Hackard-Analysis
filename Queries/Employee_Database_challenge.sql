9c6dae8c56b1974d40ad9664e406bff5c5359e65
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-06-19 07:18:44 -0400

    start deliverable 1

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,24 @@
+-- Deliverable 1: steps 1-7
+-- Get retiring employees and their titles
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+	ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Deliverable 1: steps 8-13
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no, to_date DESC;
